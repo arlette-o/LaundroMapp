6a1a2d62aa841e55454c9bb894bab8e343e485ea
Arlette Olalde <aolaldeflores@gmail.com>
2024-12-16 18:39:41 -0800

    caching mongo connection

--- utils/index.tsx
+++ utils/index.tsx
@@ -3,16 +3,45 @@ import { NextResponse } from "next/server";
 
 const URI = process.env.MONGO_URI;
 
+declare global {
+  var mongoose: any;
+}
+
+let cached = global.mongoose;
+
 const connectToMongo = async () => {
   if (!URI) throw new Error("Missing MongoDB URI");
 
-  if (mongoose.connections[0].readyState) return true;
-  try {
-    await mongoose.connect(URI, { connectTimeoutMS: 50000 });
-    console.log("Successful connection to database");
-    return true;
-  } catch (error: any) {
-    console.error("Error: Mongo connection error", error.message);
+  if (!cached) {
+    cached = global.mongoose = { conn: null, promise: null };
+  }
+
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      maxPoolSize: 2,
+      maxIdleTimeMS: 60000,
+      bufferCommands: false,
+      serverSelectionTimeoutMS: 8000, //Stay within Vercel's 10 second function limit
+      heartbeatFrequencyMS: 10000, //Attempting to see if this reduces query timeouts
+    };
+
+    console.log("---Connecting to MongoDB---");
+
+    try {
+      cached.promise = mongoose
+        .connect(process.env.MONGO_URI || "", opts)
+        .then((mongoose) => {
+          console.log("---Connected!---");
+          return mongoose;
+        });
+    } catch (e) {
+      console.log("---Error connecting to MongoDB---", e);
+      throw new Error("Error connecting to database");
+    }
   }
 };
 
